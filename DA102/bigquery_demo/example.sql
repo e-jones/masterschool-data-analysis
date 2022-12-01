48e33ed13a109d31639ba4ccbe285b9ec8e4a05f
e-jones <ejones@mavendc.com>
2022-11-30 19:45:56 -0800

    Descriptive statistics on a BQ Taxi dataset
    Looking at NYC taxi cab data and running some fun queries

--- DA102/bigquery_demo/example.sql
+++ DA102/bigquery_demo/example.sql
@@ -1,2 +1,15 @@
--- There is nothing here!
--- Let's write some SQL code
+/*
+-- Example Masterschool queries on a BigQuery Public Dataset
+-- Highest passenger Count
+-- Highest tip amount
+-- Longest trip (miles, and time)
+*/
+
+SELECT  
+  MAX(trip_distance) AS longest_distance,
+  MAX(tip_amount) AS highest_tip,
+  MAX(passenger_count) AS most_passengers,
+  AVG(trip_distance) AS avg_distance,
+  AVG(tip_amount) AS avg_tip,
+  AVG(passenger_count) AS avg_passengers
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022` 
